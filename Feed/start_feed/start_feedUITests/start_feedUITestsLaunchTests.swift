b74370b24a01615e7944e6daf29d17079dfc647c
jake <palatable7@naver.com>
2023-06-28 00:06:07 +0900

    add files

---
+++ Feed/start_feed/start_feedUITests/start_feedUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  start_feedUITestsLaunchTests.swift
+//  start_feedUITests
+//
+//  Created by 김종권 on 2023/06/28.
+//
+
+import XCTest
+
+final class start_feedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
